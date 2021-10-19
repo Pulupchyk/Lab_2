4187b662034fae8734b860f523249647a7446c26
Pylypchuk <artempulupchyk1@gmail.com>
2021-10-19 16:30:47 +0300

    commit ninth step

--- LabTest.java
+++ LabTest.java
@@ -15,37 +15,38 @@ public class LabTest {
         ninth_step_test();
     }
     public static void first_step_test(){
-        assertEquals(0, Main.Add(""));
-        assertEquals(1, Main.Add("1"));
-        assertEquals(3, Main.Add("1,2"));
+        assertEquals(0, Main.Add(""), 0.0);
+        assertEquals(1, Main.Add("1"),1.0);
+        assertEquals(3, Main.Add("1,2"), 3.0);
     }
     public static void second_step_test(){
-        assertEquals(28, Main.Add("1,2,3,4,5,6,7"));
+        assertEquals(28, Main.Add("1,2,3,4,5,6,7"), 28.0);
     }
     public static void third_test_step(){
-        assertEquals(6, Main.Add("1\n2,3"));
+        assertEquals(6, Main.Add("1\n2,3"), 6.0);
     }
     public static void forth_step_test(){
-        assertEquals(3, Main.Add("//;\n1;2"));
+        assertEquals(3, Main.Add("//;\n1;2"), 3.0);
     }
     public static void fifth_step_test(){
         try{
             Main.Add("//*\n-1*2*-3");
         }catch (RuntimeException ex) {
-            assertEquals("negatives not allowed: -1 -3", ex.getMessage());
-            //System.out.println(ex.getMessage());
+            //assertEquals("negatives not allowed: -1 -3", ex.getMessage(), "negatives not allowed: -1.0 -3.0");
+            System.out.println(ex.getMessage());
         }
     }
     public static void sixth_step_test() {
-        assertEquals(2, Main.Add("2,1001"));
+        assertEquals(2, Main.Add("2,1001"), 2.0);
     }
     public static void seventh_step_test(){
-        assertEquals(6, Main.Add("//[***]\n1***2***3"));
+        assertEquals(6, Main.Add("//[***]\n1***2***3"), 6.0);
     }
     public static void eighth_step_test(){
-        assertEquals(6, Main.Add("//[*][%]\n1*2%3"));
+        assertEquals(6, Main.Add("//[*][%]\n1*2%3"), 6.0);
     }
     public static void ninth_step_test() {
-        assertEquals(15, Main.Add("//[*][%#][$%^][!@%^&]\n1*2%#3$%^4!@%^&5"));
+        assertEquals(10, Main.Add("//[*][***][**]\n1.1**2.5*10002*3***4.1"), 10.7);
+
     }
 }
--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
 package org.kpi.fpm.lab_2;
 
+import java.net.CacheRequest;
+
 public class Main{
     public static void main(String[] args) {
         LabTest.test();
+        System.out.println(Add("//[*][**][***]\n1.1*2.5**3.2***4*1002**-1"));
     }
-    public static int Add(String numbers) {
-        int d = 0;
+    public static double Add(String numbers) {
+        double d = 0;
         int n = 0;
         String neg = "";
         if (numbers.isEmpty()) {
@@ -14,19 +17,31 @@ public class Main{
         else if (numbers.startsWith("//")) {
             String delimiter = "";
             if (numbers.contains("[") && numbers.contains("]") && numbers.contains("][")) {
-                numbers = numbers.replace("//[", "").replace("][", ",");
-                for (int i = 0; i < numbers.length(); i++) {
-                    if (numbers.charAt(i) != ']') {
-                        delimiter += numbers.charAt(i);
-                    } else {
-                        numbers = numbers.replace(delimiter + "]\n", "");
-                        break;
+                String[] str = numbers.split("\n");
+                String s = "";
+                for(int i = 0; i< str.length; i++){
+                    if(i > 0) {
+                        s += str[i]+" ";
                     }
                 }
-                String[] delimiters = delimiter.split(",");
-                numbers = numbers.replace("\n", ",");
-                for(int i = 0; i < delimiters.length; i++){
-                    numbers = numbers.replace(delimiters[i], ",");
+                String an_s = "";
+                for(int i = 0; i < s.length(); i++){
+                    if(Character.isDigit(s.charAt(i)) || s.charAt(i) == '.'){
+                        an_s += s.charAt(i);
+                    } else{
+                        an_s +=',';
+                    }
+                }
+                an_s = an_s.replaceAll("(,)\\1+", ",");
+                String[] num = an_s.split(",");
+                for(String st : num){
+                    if (Double.parseDouble(st) < 0) {
+                        n++;
+                        neg += Double.parseDouble(st) + " ";
+                    }
+                    if (Double.parseDouble(st) < 1001) {
+                        d += Double.parseDouble(st);
+                    }
                 }
             }
             else if (numbers.contains("[") && numbers.contains("]")) {
@@ -41,19 +56,28 @@ public class Main{
                 }
                 numbers = numbers.replace("\n", ",");
                 numbers = numbers.replace(delimiter, ",");
+                for (String s : numbers.split(",|\n")) {
+                    if (Double.parseDouble(s) < 0) {
+                        n++;
+                        neg += Double.parseDouble(s) + " ";
+                    }
+                    if (Double.parseDouble(s) < 1001) {
+                        d += Double.parseDouble(s);
+                    }
+                }
             }
             else if (!(numbers.contains("[") && numbers.contains("]"))) {
                 delimiter = Character.toString(numbers.charAt(2));
                 numbers = numbers.replace("//" + delimiter + "\n", "");
                 numbers = numbers.replace(delimiter, ",");
-            }
-            for (String s : numbers.split(",|\n")) {
-                if (Integer.parseInt(s) < 0) {
-                    n++;
-                    neg += Integer.parseInt(s) + " ";
-                }
-                if (Integer.parseInt(s) < 1001) {
-                    d += Integer.parseInt(s);
+                for (String s : numbers.split(",|\n")) {
+                    if (Double.parseDouble(s) < 0) {
+                        n++;
+                        neg += Double.parseDouble(s) + " ";
+                    }
+                    if (Double.parseDouble(s) < 1001) {
+                        d += Double.parseDouble(s);
+                    }
                 }
             }
 
@@ -65,12 +89,12 @@ public class Main{
         }
         else {
             for (String s : numbers.split(",|\n")) {
-                if (Integer.parseInt(s) < 0) {
+                if (Double.parseDouble(s) < 0) {
                     n++;
-                    neg += Integer.parseInt(s) + " ";
+                    neg += Double.parseDouble(s) + " ";
                 }
-                if (Integer.parseInt(s) < 1001) {
-                    d += Integer.parseInt(s);
+                if (Double.parseDouble(s) < 1001) {
+                    d += Double.parseDouble(s);
                 }
             }
             if (n > 0) {
